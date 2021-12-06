a56e1fbc7caa99ddc8b172f4cdbc21f2f5ea480b
kubota22 <msg.1817002@gmail.com>
2021-12-06 11:57:52 +0900

    main←kubotaの競合回避

--- data.js
+++ data.js
@@ -1,30 +1,3 @@
-// import koma from "koma"
-// var initTable = () => {
-//     //table 内の td 要素をすべて取り出す。
-//     const list = document.querySelectorAll("td");
-//     //td にクリックイベントを付ける。
-//     for (const td of list) {
-//         td.onclick = () => onclickTd(td);  // ()=> はアロー関数
-//     }
-// }
-
-// var onclickTd = (td) => {
-//     //テーブルをクリックしたときの動作
-
-// }
-
-// window.onload = () => {
-//     // 実行したい処理を書く
-//     initTable();
-// }
-
-// function onClick(link) {
-//     var tableId = $(link).attr('id') + $(link).attr('class');
-//     alert(tableId);
-// }
-
-
-
 var pick = document.getElementById("pick");
 
 document.addEventListener("click", function (e) {
